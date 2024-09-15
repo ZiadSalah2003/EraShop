45626bb551d2d48af51ad6330a9b7588d05a207d
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-15 04:16:43 +0300

    add configuration of token

--- EraShop.API/DependencyInjection.cs
+++ EraShop.API/DependencyInjection.cs
@@ -1,5 +1,12 @@
-﻿using EraShop.API.Persistence;
+﻿using EraShop.API.Authentication;
+using EraShop.API.Entities;
+using EraShop.API.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace EraShop.API
 {
@@ -14,6 +21,7 @@ namespace EraShop.API
 
 
 			services.AddSwaggerServices();
+			services.AddAuthConfig(configuration);
 
 			return services;
 		}
@@ -28,7 +36,40 @@ namespace EraShop.API
 
 		private static IServiceCollection AddAuthConfig(this IServiceCollection services, IConfiguration configuration)
 		{
-			
+			services.AddIdentity<ApplicationUser, ApplicationRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
+
+			services.AddSingleton<IJwtProvider, JwtProvider>();
+			services.AddOptions<JwtOptions>().BindConfiguration(JwtOptions.SectionName).ValidateDataAnnotations().ValidateOnStart();
+
+			var jwtSettings = configuration.GetSection(JwtOptions.SectionName).Get<JwtOptions>();
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+				.AddJwtBearer(o =>
+				{
+					o.SaveToken = true;
+					o.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings?.Key!)),
+						ValidIssuer = jwtSettings?.Issuer,
+						ValidAudience = jwtSettings?.Audience
+					};
+				});
+			services.Configure<IdentityOptions>(options =>
+			{
+				options.Password.RequiredLength = 6;
+				options.SignIn.RequireConfirmedEmail = true;
+				options.User.RequireUniqueEmail = true;
+			});
 
 			return services;
 		}
