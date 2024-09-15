c492e6bf33f5e7b10823490bd8aa94571a7c9582
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-15 05:04:12 +0300

    seed data for user and role in database

---
+++ EraShop.API/Persistence/EntitiesConfigrations/RoleConfigration.cs
@@ -0,0 +1,32 @@
+﻿using EraShop.API.Abstractions.Consts;
+using EraShop.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EraShop.API.Persistence.EntitiesConfigrations
+{
+	public class RoleConfigration : IEntityTypeConfiguration<ApplicationRole>
+	{
+		public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+		{
+			//Default Data
+			builder.HasData([
+				new ApplicationRole
+				{
+					Id = DefaultRoles.AdminRoleId,
+					Name = DefaultRoles.Admin,
+					NormalizedName = DefaultRoles.Admin.ToUpper(),
+					ConcurrencyStamp = DefaultRoles.AdminRoleConcurrencyStamp
+				},
+				new ApplicationRole
+				{
+					Id = DefaultRoles.UserRoleId,
+					Name = DefaultRoles.User,
+					NormalizedName = DefaultRoles.User.ToUpper(),
+					ConcurrencyStamp = DefaultRoles.UserRoleConcurrencyStamp,
+					IsDefault = true
+				}
+			]);
+		}
+	}
+}
