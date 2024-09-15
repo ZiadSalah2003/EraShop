e54cc6ce7da013719e106733467a7c8d80138335
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-15 05:04:53 +0300

    add login request and its validator

--- EraShop.API/Controllers/AuthController.cs
+++ EraShop.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EraShop.API.Contracts.Authentication;
+using EraShop.API.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EraShop.API.Controllers
@@ -7,10 +9,19 @@ namespace EraShop.API.Controllers
 	[ApiController]
 	public class AuthController : ControllerBase
 	{
-		[HttpPost("login")]
-		public IActionResult Login()
+		private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+		}
+
+        [HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody] LoginRequest request,CancellationToken cancellationToken)
 		{
-			return Ok();
+			var response = await _authService.GetTokenAsync(request.Email, request.Password, cancellationToken);
+			if (response is null)
+				return BadRequest();
+			return Ok(response);
 		}
 	}
 }
