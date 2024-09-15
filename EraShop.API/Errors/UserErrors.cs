939ed3ca5153b481eae9855a0197e11019303c25
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-15 05:34:53 +0300

    add error for excepcted user error

---
+++ EraShop.API/Errors/UserErrors.cs
@@ -0,0 +1,11 @@
+﻿using EraShop.API.Abstractions;
+
+namespace EraShop.API.Errors
+{
+	public static class UserErrors
+	{
+		public static readonly Error InvalidCredentails = new("User.InvalidCredentials", "Invalid Email Or Password", StatusCodes.Status401Unauthorized);
+
+		public static readonly Error InvalidJwtToken = new("User.InvalidJwtToken", "Invalid Jwt token", StatusCodes.Status401Unauthorized);
+	}
+}
