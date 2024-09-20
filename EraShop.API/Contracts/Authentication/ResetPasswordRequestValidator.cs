fa119dffd40f6ca892209ef46bf26b8198bc2666
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-20 05:40:07 +0300

    add request and it's validation for forget password & reset password

---
+++ EraShop.API/Contracts/Authentication/ResetPasswordRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using EraShop.API.Abstractions.Consts;
+using FluentValidation;
+
+namespace EraShop.API.Contracts.Authentication
+{
+	public class ResetPasswordRequestValidator : AbstractValidator<ResetPasswordRequest>
+	{
+		public ResetPasswordRequestValidator()
+		{
+			RuleFor(x => x.Email)
+				.EmailAddress()
+				.NotEmpty();
+
+			RuleFor(x => x.Code)
+				.NotEmpty();
+
+			RuleFor(x => x.NewPassword)
+				.NotEmpty()
+				.Matches(RegexPatterns.Password)
+				.WithMessage("Passwrod should be at least 8 digits and should contains lower case, nonalphanumeric and uppercase");
+
+		}
+	}
+}
