75b089180f44a032b993fb6e57b3aa6e1098e4a7
Mohamed-Khaled970 <mohamed.khaled.cg@gmail.com>
2024-12-05 22:25:14 +0200

    Adding Image Column in Brand Table

--- EraShop.API/Entities/Brand.cs
+++ EraShop.API/Entities/Brand.cs
@@ -5,6 +5,7 @@
 		public int Id { get; set; }
 		public string Name { get; set; } = string.Empty;
 		public bool IsDisable { get; set; }
-		public ICollection<Product> Products { get; set; } = [];
+        public string? ImageUrl { get; set; } = string.Empty;
+        public ICollection<Product> Products { get; set; } = [];
 	}
 }
---
+++ EraShop.API/Migrations/20241205202243_AddImageColumnInBrandTable.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EraShop.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageColumnInBrandTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Brands",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6dc6528a-b280-4770-9eae-82671ee81ef7",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAED2XBrDiOSE5wPYkQBXFi2yAxeFzBcGQwfMna00c5VMMNgBkauj0zTOKGDsVZJu07w==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Brands");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6dc6528a-b280-4770-9eae-82671ee81ef7",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEEPPtSOyFCOqGsC1BsqNPSCN213CkHrMlXYVsGm83tW96IcIFXh7jeNiQJl/Wj4L3A==");
+        }
+    }
+}
