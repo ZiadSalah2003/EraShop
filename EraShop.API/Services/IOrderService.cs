fb1116ccf00b59d338172e521fd156fe670b68e4
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-02-15 01:51:04 +0200

    add order implementation and endpoints

---
+++ EraShop.API/Services/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using EraShop.API.Contracts.Orders;
+
+namespace EraShop.API.Services
+{
+	public interface IOrderService
+	{
+		public Task<Result<OrderResponse>> CreateOrderAsync(string buyerEmail, OrderCreateRequest request);
+		public Task<Result<OrderResponse>> GetOrdersForUserAsync(string buyerEmail);
+		public Task<Result<OrderResponse>> GetOrderByIdAsync(string buyerEmail, int orderId);
+		public Task<Result<IEnumerable<DeliveryMethodResponse>>> GetDeliveryMethodsAsync();
+	}
+}
