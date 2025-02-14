3f66adfce57005de3766698290006a43561ee291
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-02-15 01:51:56 +0200

    add payment endpoint and implementation

---
+++ EraShop.API/Controllers/PaymentController.cs
@@ -0,0 +1,31 @@
+﻿using EraShop.API.Abstractions;
+using EraShop.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EraShop.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PaymentController : ControllerBase
+	{
+		private readonly IPaymentService _paymentService;
+		public PaymentController(IPaymentService paymentService)
+		{
+			_paymentService = paymentService;
+		}
+		[HttpPost("{basketId}")]
+		public async Task<IActionResult> CreateOrUpdatePaymentIntent(string basketId)
+		{
+			var response = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+			return response.IsSuccess ? Ok(response.Value) : response.ToProblem();
+		}
+		[HttpPost("webhook")]
+		public async Task<IActionResult> WebHook()
+		{
+			var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+			await _paymentService.UpdateOrderPaymentStatus(json, Request.Headers["Stripe-Signature"]!);
+			return Ok();
+		}
+	}
+}
