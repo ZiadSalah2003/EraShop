44d9b3e38f32451d8683b9c8693e1e53217a9c43
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-02-15 01:53:38 +0200

    add get user email and add errors for user

---
+++ EraShop.API/Extensions/UserExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace EraShop.API.Extensions
+{
+	public static class UserExtensions
+	{
+		public static string? GetUserId(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+		public static string? GetUserEmail(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.Email);
+		}
+	}
+}
---
+++ EraShop.API/Settings/StripeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace EraShop.API.Settings
+{
+	public class StripeSettings
+	{
+		public required string SecretKey { get; set; }
+		public required string WebhookSecret { get; set; }
+
+	}
+}
