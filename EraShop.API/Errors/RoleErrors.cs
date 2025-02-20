3ba3751d1bd1c7ac7d777a0995ac63a459f21e7b
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-02-20 19:40:07 +0200

    add role error for exceptions

---
+++ EraShop.API/Errors/RoleErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace EraShop.API.Errors
+{
+	public static class RoleErrors
+	{
+		public static readonly Error RoleNotFound = new("Role.NotFound", "Role is not found", StatusCodes.Status404NotFound);
+		public static readonly Error DuplicatedRole = new("Role.DuplicatedRole", "Another role with the same name is already exists", StatusCodes.Status409Conflict);
+	}
+}
