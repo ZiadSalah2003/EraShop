7dddc7187976ce149803b0fcd6ef23c2cc09ea30
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-03-06 21:35:28 +0200

    add get refresh token

--- EraShop.API/Services/IAuthService.cs
+++ EraShop.API/Services/IAuthService.cs
@@ -7,6 +7,7 @@ namespace EraShop.API.Services
 	{
 		Task<Result<AuthResponse>> GetTokenAsync(string email, string password, CancellationToken cancellationToken = default);
 		Task<Result> RevokeRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
+		Task<Result<AuthResponse>> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default)
 		Task<Result> SignUpAsync(SignUpRequest request, CancellationToken cancellationToken = default);
         Task<Result> ConfirmEmailAsync(ConfirmEmailRequest request);
 		Task<Result> ResendConfirmationEmailAsync(ResendEmailConfirmationRequest request);
