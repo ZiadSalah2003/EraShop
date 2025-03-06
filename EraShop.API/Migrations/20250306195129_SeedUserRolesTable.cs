ca0e40dfce06d61662b76be6a998722f7df4a89f
Mohamed-Khaled970 <mohamed.khaled.cg@gmail.com>
2025-03-06 21:57:31 +0200

    seed user roles configurations

---
+++ EraShop.API/Migrations/20250306195129_SeedUserRolesTable.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EraShop.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedUserRolesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "92b75286-d8f8-4061-9995-e6e23ccdee94", "6dc6528a-b280-4770-9eae-82671ee81ef7" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6dc6528a-b280-4770-9eae-82671ee81ef7",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAELxJSMwD2v4uG61TUnAMqLJwjOW+oowI6JFuTF+XR0betfmSinQWsjunWe5ffE8XGg==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "92b75286-d8f8-4061-9995-e6e23ccdee94", "6dc6528a-b280-4770-9eae-82671ee81ef7" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6dc6528a-b280-4770-9eae-82671ee81ef7",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEAR2V+bcDJAlzUiuTRqKkLj/Uv4ibKCWikvvMF1g75/iOokLhV1l9SedoJOqspT0mA==");
+        }
+    }
+}
---
+++ EraShop.API/Persistence/EntitiesConfigrations/UserRoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using EraShop.API.Abstractions.Consts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EraShop.API.Persistence.EntitiesConfigrations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            //Default Data
+            builder.HasData
+            (
+                new IdentityUserRole<string>
+                {
+                    UserId = DefaultUsers.AdminId,
+                    RoleId = DefaultRoles.AdminRoleId
+                }
+            );
+        }
+    }
+}
