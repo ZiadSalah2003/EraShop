4763cea3bdf6d4523657656ed56878280025364a
Mohamed-Khaled970 <mohamed.khaled.cg@gmail.com>
2025-03-08 00:54:31 +0200

    Add Validations When Create Or Update Basket

---
+++ EraShop.API/Contracts/Baskets/BasketItemResponseValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+
+namespace EraShop.API.Contracts.Baskets
+{
+    public class BasketItemResponseValidator : AbstractValidator<BasketItemResponse>
+    {
+        public BasketItemResponseValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(x => x.ProductName)
+                .NotNull()
+               .NotEmpty();
+
+            RuleFor(x => x.PictureUrl)
+                .NotNull()
+               .NotEmpty();
+
+            RuleFor(x => x.Price)
+               .NotEmpty();
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+               .NotEmpty();
+
+            RuleFor(x => x.Brand)
+                .NotNull()
+               .NotEmpty();
+
+            RuleFor(x => x.Category)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
