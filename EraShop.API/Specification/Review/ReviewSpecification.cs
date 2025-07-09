a887767b6b20d0a2cfab32ac680ee678614adf5d
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-07-09 03:04:52 +0300

    Implement repository and specification pattern with generic interfaces and classes

---
+++ EraShop.API/Specification/Review/ReviewSpecification.cs
@@ -0,0 +1,37 @@
+using EraShop.API.Entities;
+using System.Linq.Expressions;
+
+namespace EraShop.API.Specification.Review
+{
+    public class ReviewSpecification : Specification<EraShop.API.Entities.Review, int>
+    {
+        public ReviewSpecification() : base()
+        {
+            AddOrderBy(r => r.Id);
+            AddIncludes();
+        }
+        
+        public ReviewSpecification(Expression<Func<EraShop.API.Entities.Review, bool>>? expression) : base(expression)
+        {
+            AddOrderBy(r => r.Id);
+            AddIncludes();
+        }
+
+        public ReviewSpecification(int id) : base(r => r.Id == id)
+        {
+            AddIncludes();
+        }
+
+        public ReviewSpecification(string userId) : base(r => r.UserId == userId)
+        {
+            AddOrderBy(r => r.Id);
+            AddIncludes();
+        }
+
+        private protected override void AddIncludes()
+        {
+            Includes.Add(r => r.Product);
+            Includes.Add(r => r.User);
+        }
+    }
+} 
